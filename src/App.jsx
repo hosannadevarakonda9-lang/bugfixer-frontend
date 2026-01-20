79ac0119548510be30b5474dfcbad9dd91ef86eb
hosanna <hosannadevarakonda9@gmail.com>
2026-01-20 09:50:49 +0530

    improve login button ux and clean up login code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-       
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-       <h1><b>Welcome to The BugFixer Project</b></h1>
     </BrowserRouter>
   );
 }
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,9 +3,25 @@ import Layout from "../components/layout/Layout";
 const Dashboard = () => {
   return (
     <Layout>
-      <h1 className="text-2xl font-semibold">
+        <div className="space-y-6">
+      <h1 className="text-2xl font-bold">
         Welcome to BugFixer Dashboard
       </h1>
+
+      <div className="grid grid-cols-3 gap-4">
+        <div className="bg-white shadow p-4 rounded">
+          Total Issues: 24
+        </div>
+
+        <div className="bg-white shadow p-4 rounded">
+          Open Issues: 10
+        </div>
+
+        <div className="bg-white shadow p-4 rounded">
+          Closed Issues: 14
+        </div>
+      </div>
+    </div>
     </Layout>
   );
 };
